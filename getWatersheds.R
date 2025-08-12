12c012bbe188717f99b63933d51aa12e6cde2e8c
kathryn-willi <kathryn.willi@colostate.edu>
2025-08-12 16:50:50 -0600

    faster version

--- getWatersheds.R
+++ getWatersheds.R
@@ -8,7 +8,8 @@
 #' @return Watershed shapefile(s) for the aoi
 #' 
 getWatersheds <- function(aoi){
-  
+  # aoi <- sf::st_read("data/ClarkCounty_NV/ClarkCounty_NV.shp")
+ 
   # read in the complete NHD (in tabular form) to make for much more efficient nhd crawling. 
   # This data in tabular form doesn't exist anywhere online that I know of... -_-
   nhd <- data.table::fread('data/nhd_flow_network.csv') 
@@ -16,7 +17,7 @@ getWatersheds <- function(aoi){
   # download flowlines for entire aoi
   nhd_flowlines <- nhdplusTools::get_nhdplus(AOI = aoi, 
                                              realization='flowline',
-                                             t_srs = st_crs(aoi))
+                                             t_srs = sf::st_crs(aoi))
   
   # add `tocomid` field to ID flowlines that cross over the aoi
   # this step massively speeds up run time by reducing the number
@@ -30,91 +31,88 @@ getWatersheds <- function(aoi){
     dplyr::filter(tocomid==0) %>%
     tibble::rowid_to_column(., "index")
   
-  # FUNCTION THAT, FOR EVERY "OUTSIDER" POUR POINT, IDENTIFIES ALL UPSTREAM FLOWLINES
-  watersheds <- function(spid_union) {
+  # if("Colorado River" %in% outsiders$gnis_name){
+  #   
+  #   mainstem <- outsiders %>%
+  #     filter(gnis_name == "Colorado River")
+  
+  watersheds <- vector("list", length = nrow(outsiders))
+  
+  for(i in 1:nrow(outsiders)){
     
-    tracer <- function(samples) {
-      outsiders <- as_tibble(outsiders)
-      outlet <- outsiders %>%
-        dplyr::filter(index == samples)
-      
-      upstream <- nhdplusTools::get_UT(nhd, outlet$comid) %>% #upstream trace function in nhdplusTools
-        tibble::as_tibble() %>%
-        dplyr::rename(comid_list = value) %>%
-        dplyr::distinct(comid_list, .keep_all = TRUE) %>%
-        dplyr::filter(comid_list != outlet$comid)
-    }
-    
-    ws <- purrr::map(spid_union, tracer) %>% dplyr::bind_rows()
+    watersheds[[i]] <- nhdplusTools::get_nldi_basin(list("comid", as.character(outsiders$comid[i])))
     
   }
   
-  park_ws <- outsiders %>%
-    dplyr::mutate(comid_list = map(index, watersheds)) %>%
-    tidyr::unnest(cols = comid_list) %>%
-    dplyr::distinct(comid_list) %>%
-    sf::st_drop_geometry() %>%
-    dplyr::rename(comid = comid_list)
-  
-  splitter_flowlines <- nhdplusTools::get_nhdplus(comid = outsiders$comid,
-                                                  realization = 'flowline',
-                                                  t_srs = st_crs(aoi)) %>%
-    dplyr::select(comid)
-  
-  nhd_flowlines <-  park_ws$comid %>%
-    purrr::map(~nhdplusTools::get_nhdplus(comid = .,
-                                          realization='flowline',
-                                          t_srs = st_crs(aoi)) %>% dplyr::select(comid)) %>%
-    dplyr::bind_rows() %>%
-    #dplyr::select(comid) %>%
-    dplyr::bind_rows(splitter_flowlines) %>%
-    dplyr::distinct(comid,.keep_all=TRUE)
-  
-  
-  grouper <- igraph::components(igraph::graph.adjlist(sf::st_touches(nhd_flowlines)))[[1]] %>%
-    tibble::as_tibble() %>%
-    dplyr::rename(relationship = value)
-  
-  grouped_flowlines <- dplyr::bind_cols(nhd_flowlines, grouper) %>% 
-    dplyr::group_by(relationship) %>%
-    dplyr::summarize()
-  
-  # include catchments in the park that are not contained in trace (no NHD flowlines associated with them)
-  nhd_empty_catchments <- nhdplusTools::get_nhdplus(AOI = aoi, 
-                                                    realization='catchment',
-                                                    t_srs = st_crs(aoi)) %>%
-    dplyr::distinct(featureid, .keep_all=TRUE) %>%
-    dplyr::filter(!featureid %in% nhd_flowlines$comid) %>%
-    dplyr::filter(!featureid %in% outsiders$comid) %>%
-    dplyr::mutate(relationship = 0) %>%
-    dplyr::select(relationship) %>%
-    sf::st_intersection(aoi)
-  
-  splitters <- nhdplusTools::get_nhdplus(comid = outsiders$comid,
-                                         realization = 'catchment',
-                                         t_srs = st_crs(aoi)) %>%
-    sf::st_intersection(aoi)
-  
-  sf_use_s2(FALSE)
-  
-  grouped_catchments <- park_ws$comid %>%
-    purrr::map(~nhdplusTools::get_nhdplus(comid = .,
-                                          realization = 'catchment',
-                                          t_srs = st_crs(aoi))) %>%
-    dplyr::bind_rows() %>%
-    dplyr::filter(!featureid %in% outsiders$comid) %>%
-    dplyr::bind_rows(splitters) %>%
-    sf::st_join(.,grouped_flowlines) %>%
-    dplyr::bind_rows(nhd_empty_catchments) %>%
-    dplyr::group_by(relationship) %>%
+  sf::sf_use_s2(FALSE)
+  main_watershed <- dplyr::bind_rows(watersheds) %>%
+    sf::st_make_valid() %>%
     dplyr::summarize() %>%
+    nngeo::st_remove_holes()
+  
+  if(st_crs(main_watershed) != st_crs(aoi)){
+    main_watershed <- main_watershed %>%
+      st_transform(., st_crs(aoi))
+  }
+  
+  sf::sf_use_s2(TRUE)
+  
+  sub_ws <- vector("list", length(nrow(outsiders)))
+  
+  for(i in 1:nrow(outsiders)){
+    sub_ws[[i]] <- nhdplusTools::get_DM(network = nhd,
+                                        comid = outsiders$comid[i],
+                                        distance = 35) %>%
+      as_tibble() %>%
+      filter(value %in% nhd_flowlines$comid) %>%
+      mutate(catchment = outsiders$comid[i])
+  }
+  
+
+  # if no downstream segments in the AOI = escaping flowline OR something weird
+  funky_ws <- bind_rows(sub_ws) %>%
+    group_by(catchment) %>%
+    summarize(count = n()) %>%
+    filter(count == 1)
+  
+  funk_finder <- vector("list", length = nrow(funky_ws))
+  
+  # of those mystery locs...
+  for(i in 1:nrow(funky_ws)){
+    funk_finder[[i]] <- nhdplusTools::get_UT(network = nhd,
+                                             comid = funky_ws$catchment[i],
+                                             distance = 35) %>%
+      as_tibble() %>%
+      #filter(value %in% nhd_flowlines$comid) %>%
+      mutate(catchment = funky_ws$catchment[i])
+  }
+  
+  # Those that have no upstream or downstream segments inside the aoi
+  # are strange. Remove their extents outside of the AOI.
+  mystery_flowlines <- bind_rows(funk_finder) %>%
+    filter(value %in% nhd_flowlines$comid) %>%
+    group_by(catchment) %>%
+    mutate(count = n()) %>%
+    filter(count == 1) 
+  
+  mystery_cats <- get_nhdplus(comid = mystery_flowlines$catchment, realization = "catchment",
+                              t_srs = sf::st_crs(aoi))
+  
+  lil_cats_removed <- main_watershed %>%
+    st_difference(., st_union(mystery_cats))
+  
+  final_ws <- lil_cats_removed %>%
+    bind_rows(st_make_valid(aoi)) %>%
+    summarize() %>%
     nngeo::st_remove_holes() %>%
-    sf::st_join(aoi) %>%
-    dplyr::select(-relationship) %>%
-    dplyr::summarize()
+    st_make_valid() %>%                           # guard against invalid rings
+    st_cast("POLYGON") %>%                        # split MULTIPOLYGON into rows
+    mutate(.area = as.numeric(st_area(geometry))) %>% 
+    slice_max(.area, n = 1, with_ties = FALSE) %>% 
+    select(-.area)
   
-  sf_use_s2(TRUE)
+  gc()
   
-  return(grouped_catchments)
-  
+  return(final_ws)
+
 }
